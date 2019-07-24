2f995eeeb91bc9377e1e96de31492bb966319a72
jinyanan <jinyanan@u51.com>
2019-07-24 16:11:51 +0800

    英文国际化

--- src/locales/en-US/menu.ts
+++ src/locales/en-US/menu.ts
@@ -47,4 +47,6 @@ export default {
   'menu.editor.flow': 'Flow Editor',
   'menu.editor.mind': 'Mind Editor',
   'menu.editor.koni': 'Koni Editor',
+  'menu.index': 'Index',
+  'menu.title': 'Blog',
 };
