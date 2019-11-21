7b90e0647f048acff545f2499c12670790df962f
jinyanan <jinyanan@u51.com>
2019-11-21 15:52:47 +0800

    博文标签

--- src/components/PostTitle/index.tsx
+++ src/components/PostTitle/index.tsx
@@ -1,12 +1,13 @@
 import React, { PureComponent, ReactNode } from 'react';
 import { PostInfo } from '@/models/post';
-import { Card } from 'antd';
+import { Card, List } from 'antd';
 import Meta from 'antd/lib/card/Meta';
 import moment from 'moment';
 import IconText from '@/components/IconText';
 import Tag from '@/components/Tag';
 import { Link } from 'umi';
 import styles from './index.less';
+import { TagType } from '@/entity/tag';
 
 interface PostTitleProps {
   post: PostInfo;
@@ -31,8 +32,17 @@ class PostTitle extends PureComponent<PostTitleProps> {
                 </div>
               </div>
               <div className={styles.postTitleTags}>
-                <IconText type="read-o" text={<Tag text={post.readTimes} />} />
-                <IconText type="like-o" text={<Tag text={post.likedTimes} />} />
+                <IconText
+                  type="crown"
+                  text={
+                    <List<TagType>
+                      dataSource={post.tagList}
+                      className={styles.tagList}
+                      renderItem={tag => <Tag text={tag.name} tagId={Number(tag.code)} />}
+                    />
+                  }
+                />
+                <IconText type="read" text={<Tag text={post.readTimes} />} />
               </div>
             </div>
           }
