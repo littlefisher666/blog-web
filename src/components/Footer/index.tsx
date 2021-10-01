f418aba026d44eed19d02ce01a779593bd6aadae
jinyanan <littlefisher666@163.com>
2021-10-01 08:44:35 +0800

    域名备案相关修改内容

--- src/components/Footer/index.tsx
+++ src/components/Footer/index.tsx
@@ -9,7 +9,7 @@ class Footer extends Component {
       <div className={styles.footer}>
         <div className={styles.footerLeft}>
           © 2019 LittleFisher 版权所有 ICP证：
-          <a href="http://www.beian.miit.gov.cn/">浙ICP备19017253号</a>
+          <a href="https://beian.miit.gov.cn">浙ICP备19017253号</a>
         </div>
       </div>
     );
